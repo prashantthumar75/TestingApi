63936190c62562863811863c6b8b08a73348711c
prashantthumar75 <48546055+prashantthumar75@users.noreply.github.com>
2020-11-26 12:51:22 +0530

    modify requirements.txt

--- django_quiz/essay/models.py
+++ django_quiz/essay/models.py
@@ -3,7 +3,6 @@ from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django_quiz.quiz.models import Question
 
-
 @python_2_unicode_compatible
 class Essay_Question(Question):
 
